1d924a2682dca9dee3e567cdb6b2491c7ddbfcbf
xathequann <87913306+xathequann@users.noreply.github.com>
2021-07-25 02:43:09 +0700

    Initialize XaTheQuan

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: Anh Tạo Web Này Ra
+    Chỉ Để Nói 
+    Là Anh Yêu 
+    Em Rất Nhiều 
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: ,' Em À  😙',
+    desc:  ,' Làm Người Yêu Anh Nhé 😘 ',
+    btnYes: 'Em Đồng Ý <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại Vì Anh Đẹp Trai Đó  <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Giờ Thì Nhắn Tin Cho Anh Nhé ❤.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/XaTheQuan.VietNam 
+}
